d93243d15d7036ad3e5c1a9b201aeae2337b2712
강지원 <78709765+betterplaywon@users.noreply.github.com>
2023-06-21 14:12:14 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    var answer = 0;
+    
+    d.sort((a,b)=> a-b)
+    
+    for(let i=0; i<d.length; i++) {
+        if(budget < d[i]) return answer;
+        budget -= d[i];
+        answer += 1;
+    }
+    
+    return answer;
+}
