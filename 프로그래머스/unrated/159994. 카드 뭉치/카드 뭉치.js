8e5ddb3d9be2940f4358856ddd99f8505fd58c7d
강지원 <78709765+betterplaywon@users.noreply.github.com>
2023-10-05 22:34:30 +0900

    [unrated] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,15 @@
+function solution(cards1, cards2, goal) {
+    let count = 0;
+    
+    for(let i=0; i<goal.length; i++) {
+        if(cards1[0] === goal[i]) {
+            cards1.shift();
+            count++;
+        } else if(cards2[0] === goal[i]) {
+            cards2.shift();
+            count++;
+        }
+    }
+    
+    return count === goal.length ? 'Yes' : 'No';
+}
