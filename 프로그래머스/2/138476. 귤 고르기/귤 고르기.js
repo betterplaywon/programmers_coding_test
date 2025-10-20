d7d856b2584e8fb5dcbca816bbfd8ebc0abccbfa
강지원 <78709765+betterplaywon@users.noreply.github.com>
2025-10-20 14:45:44 +0900

    [level 2] Title: 귤 고르기, Time: 51.81 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,19 +1,4 @@
-function solution(k, tangerines) {
-    const map = new Map();
-    for(let tangerine of tangerines) {
-        map.set(tangerine, map.get(tangerine) ? map.get(tangerine)+1 : 1);
-    }
-    const mappingArray = [...map];
-    let count=0;
-    mappingArray.sort((a,b)=> b[1] - a[1]);
-    
-    for(const element of mappingArray) {
-         count++;
-        if(k>element[1]) {
-           k-=element[1];
-        } else {
-            break;
-        }
-    }
-    return count;
+function solution(k, tangerine) {
+    var answer = 0;
+    return answer;
 }
